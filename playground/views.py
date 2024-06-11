d74e63767a87d0fb23d3cb200076c54b636d5da5
sanadsadhik <sanadsadhik@gmail.com>
2024-06-11 15:56:41 +0530

    views, mapping URL to views

---
+++ playground/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def say_hello(request):
+    # return HttpResponse('Hello world')
+    return render(request, 'hello.html')
+    # return render(request, 'hello.html', { 'name' : 'Sanad'})
